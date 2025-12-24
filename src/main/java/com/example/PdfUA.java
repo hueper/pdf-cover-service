af739e45d84a1c77f7ed4f327e101e91934d023a
moritz <moritz.hueper@posteo.net>
2025-12-24 13:23:28 +0100

    extract image from pdf and use that to generate the ua-1 compliant cover pdf

--- src/main/java/com/example/PdfUA.java
+++ src/main/java/com/example/PdfUA.java
@@ -1,58 +1,110 @@
 package com.example;
 
-import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.geom.PageSize;
-import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfName;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfStream;
 import com.itextpdf.kernel.pdf.PdfUAConformance;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.xobject.PdfImageXObject;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Image;
 import com.itextpdf.pdfua.PdfUAConfig;
 import com.itextpdf.pdfua.PdfUADocument;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
 
 public class PdfUA {
-    public static final String DEST = "./target/sandbox/pdfua/pdf_ua.pdf";
-    public static final String COVER = "./src/main/resources/img/image.png";
-    public static final String SOURCE_PDF = "./src/main/resources/pdf/oa9783839431597.pdf";
+
+    private static final String DEFAULT_LANGUAGE = "en-US";
+    private static final String DEFAULT_TITLE = "Book Cover";
+    private static final String COVER_ALT_TEXT = "Cover";
 
     public static void main(String[] args) throws Exception {
-        File file = new File(DEST);
-        file.getParentFile().mkdirs();
-        new PdfUA().manipulatePdf(DEST);
+        Path dest = Path.of("./target/sandbox/pdfua/pdf_ua.pdf");
+        Path source = Path.of("./src/main/resources/pdf/oa9783839431597.pdf");
+
+        Files.createDirectories(dest.getParent());
+
+        new PdfUA().createCoverPdf(source, dest);
     }
 
-    public void manipulatePdf(String dest) throws IOException {
-        // Read page size from the source PDF
-        PageSize pageSize = getPageSizeFromPdf(SOURCE_PDF);
-
-        PdfDocument pdfDoc = new PdfUADocument(new PdfWriter(dest),
-                new PdfUAConfig(PdfUAConformance.PDF_UA_1, "Some title", "en-US"));
-        Document document = new Document(pdfDoc, pageSize);
-
-        // Remove margins
-        document.setMargins(0, 0, 0, 0);
-
-        Image img = new Image(ImageDataFactory.create(COVER));
-        img.getAccessibilityProperties().setAlternateDescription("Cover");
-
-        // Scale image to fit the full page
-        img.scaleToFit(pageSize.getWidth(), pageSize.getHeight());
-        img.setFixedPosition(0, 0);
-
-        document.add(img);
-
-        document.close();
+    public void createCoverPdf(Path sourcePdfPath, Path destPath) throws IOException {
+        createCoverPdf(sourcePdfPath, destPath, DEFAULT_TITLE, DEFAULT_LANGUAGE);
     }
 
-    private PageSize getPageSizeFromPdf(String pdfPath) throws IOException {
-        try (PdfDocument sourcePdf = new PdfDocument(new PdfReader(pdfPath))) {
-            Rectangle rect = sourcePdf.getFirstPage().getPageSize();
-            return new PageSize(rect);
+    public void createCoverPdf(Path sourcePdfPath, Path destPath,
+                               String title, String language) throws IOException {
+        try (PdfDocument sourcePdf = new PdfDocument(new PdfReader(sourcePdfPath.toFile()))) {
+            PdfPage firstPage = sourcePdf.getFirstPage();
+            PageSize pageSize = new PageSize(firstPage.getPageSize());
+
+            PdfImageXObject sourceImage = extractFirstImage(firstPage)
+                    .orElseThrow(() -> new ImageExtractionException(
+                            "No image found on the first page: " + sourcePdfPath));
+
+            writeCoverPdf(destPath, pageSize, sourceImage, title, language);
+        }
+    }
+
+    private void writeCoverPdf(Path destPath, PageSize pageSize,
+                               PdfImageXObject sourceImage,
+                               String title, String language) throws IOException {
+        PdfUAConfig config = new PdfUAConfig(PdfUAConformance.PDF_UA_1, title, language);
+
+        try (PdfDocument pdfDoc = new PdfUADocument(new PdfWriter(destPath.toFile()), config);
+             Document document = new Document(pdfDoc, pageSize)) {
+
+            document.setMargins(0, 0, 0, 0);
+
+            // Copy the image to the new document
+            PdfImageXObject copiedImage = copyImageToDocument(sourceImage, pdfDoc);
+
+            Image coverImage = createFullPageImage(copiedImage, pageSize);
+            document.add(coverImage);
+        }
+    }
+
+    private PdfImageXObject copyImageToDocument(PdfImageXObject sourceImage, PdfDocument targetDoc) {
+        PdfStream copiedStream = (PdfStream) sourceImage.getPdfObject().copyTo(targetDoc);
+        return new PdfImageXObject(copiedStream);
+    }
+
+    private Image createFullPageImage(PdfImageXObject imageXObject, PageSize pageSize) {
+        Image image = new Image(imageXObject);
+        image.getAccessibilityProperties().setAlternateDescription(COVER_ALT_TEXT);
+        image.scaleToFit(pageSize.getWidth(), pageSize.getHeight());
+        image.setFixedPosition(0, 0);
+        return image;
+    }
+
+    private Optional<PdfImageXObject> extractFirstImage(PdfPage page) {
+        var xObjects = page.getResources().getResource(PdfName.XObject);
+        if (xObjects == null) {
+            return Optional.empty();
+        }
+
+        for (PdfName name : xObjects.keySet()) {
+            PdfStream stream = xObjects.getAsStream(name);
+            if (isImageStream(stream)) {
+                return Optional.of(new PdfImageXObject(stream));
+            }
+        }
+        return Optional.empty();
+    }
+
+    private boolean isImageStream(PdfStream stream) {
+        return stream != null && PdfName.Image.equals(stream.getAsName(PdfName.Subtype));
+    }
+
+    public static class ImageExtractionException extends IOException {
+        public ImageExtractionException(String message) {
+            super(message);
         }
     }
 }
